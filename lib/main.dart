10db9ac60e5831b6ea602e1ebb1be222e54a0ec5
Ashkar <kaashkarka@gmail.com>
2020-08-05 22:45:39 +0530

    add stateless widget, text styles and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Text('Thasleema`s app'),
+void main() => runApp(MaterialApp(
+    home: Home(),
   ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset('assets/space.jpg'),
+//        child: Image(
+//          image: AssetImage('assets/space.jpg'),
+        ),
+//        child: Text(
+//          'Hi App',
+//          style: TextStyle(
+//            fontSize: 20.0,
+//            fontWeight: FontWeight.bold,
+//            letterSpacing: 2.0,
+//            color: Colors.grey[600],
+//            fontFamily: 'IndieFlower',
+//          ),
+//        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click me'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
 }
 
 
